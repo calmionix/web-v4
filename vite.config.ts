0c3f5c98eb7e5b116f0d42886523d945175669f6
calmionix <calmionix@gmail.com>
2026-03-10 07:29:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,5 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/web-v4/',
-  plugins: [inspectAttr(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  base: '/web-v4/'
+})
